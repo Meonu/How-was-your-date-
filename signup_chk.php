2b4e4f82209cb3478433ec620a07f314466f0ebf
meonu <yeonugim42@gmail.com>
2022-12-29 17:48:52 +0900

    make signup

--- signup_chk.php
+++ signup_chk.php
@@ -23,27 +23,28 @@ $userid = htmlentities($_POST["userid"]);
 $userpw = htmlentities($_POST["userpw"]);
 
 $sql = "SELECT * FROM mem_info where userid='$userid'";
-    $result = mysqli_fetch_array(mysqli_query($connect, $sql));
+$result = mysqli_fetch_array(mysqli_query($connect, $sql));
 
 $hashedPassword = password_hash($_POST['userpw'], PASSWORD_DEFAULT);
 $sql = "insert into mem_info(userid,userpw) VALUES('$userid','$hashedPassword')";
 
 
-    if(mysqli_query($connect,$sql) === false)
+   
+    if($result){
+        echo "<span style='color:red;'>$userid</span> 는 중복된 아이디입니다.";
+        ?><p><input type=button value="다른 ID 사용" onclick="location.href='./SignUp.html'"></p><?php
+    }
+    else if(mysqli_query($connect,$sql) === false)
     {
         echo "오류가 발생했습니다.";
         return false;
-    }
-    else if($result){
-        echo "<span style='color:red;'>$userid</span> 는 중복된 아이디입니다.";
-        ?><p><input type=button value="다른 ID 사용" onclick="location.href='./SignUp.html'"></p><?php
     }
     else
     {   
     ?>
         <script>
         alert("회원가입이 완료되었습니다.");
-        location.href="./index.php";
+        location.href="./loginpage.php";
         </script>   
     <?php
     }
