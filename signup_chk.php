0200e50c918c1a3303a9cf7e5f0c1ab16d8e3d35
meonu <yeonugim42@gmail.com>
2022-12-29 17:43:46 +0900

    make signup

--- signup_chk.php
+++ signup_chk.php
@@ -18,10 +18,12 @@
      echo "<h2>서버와의 연결 실패</h2>";
 
 
-$userid = $_POST["decide_id"];
+$userid = htmlentities($_POST["userid"]);
 
 $userpw = htmlentities($_POST["userpw"]);
 
+$sql = "SELECT * FROM mem_info where userid='$userid'";
+    $result = mysqli_fetch_array(mysqli_query($connect, $sql));
 
 $hashedPassword = password_hash($_POST['userpw'], PASSWORD_DEFAULT);
 $sql = "insert into mem_info(userid,userpw) VALUES('$userid','$hashedPassword')";
@@ -31,6 +33,10 @@ $sql = "insert into mem_info(userid,userpw) VALUES('$userid','$hashedPassword')"
     {
         echo "오류가 발생했습니다.";
         return false;
+    }
+    else if(!$result){
+        echo "<span style='color:red;'>$userid</span> 는 중복된 아이디입니다.";
+        ?><p><input type=button value="다른 ID 사용" onclick="location.href=''"></p><?php
     }
     else
     {   
