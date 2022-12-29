6e3400cd5197e5f485f5097ae9b1ed86fa0e077b
meonu <yeonugim42@gmail.com>
2022-12-29 22:45:17 +0900

    edit

--- post.js
+++ post.js
@@ -2,8 +2,13 @@ function check(){
     var userid = document.getElementById("date").value;
     var userpw = document.getElementById("how").value;
 
+    var avoid = /^[0-9]{2}\/[0-9]{2}\/[0-9]{2}$/;
+
+    if(userid.length != 0 && avoid.test(userid) == false ){
+        alert("22/12/25 형식으로 입력해주세요.");
+    }
     var http = new XMLHttpRequest();
- 
+    
     var url = 'posting.php';
  
     var params = "date="+encodeURI(userid)+"&how="+encodeURI(userpw);
