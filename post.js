e024bbf30dfaf18563a53c87b34cd9e4381ab5e7
Yeonwoo Kim <yeonwookim@Yeonwoos-MacBook-Air.local>
2022-12-31 08:53:54 +0900

    mac edit

--- post.js
+++ post.js
@@ -2,8 +2,13 @@ function check(){
     var userid = document.getElementById("date").value;
     var userpw = document.getElementById("how").value;
 
+    var uid = '<%=(String)session.getAttribute("userid")%>';
     var avoid = /^[0-9]{2}\/[0-9]{2}\/[0-9]{2}$/;
-
+    if(uid.length == 0)
+    {
+        alert("로그인이 필요합니다.");
+        return 0;
+    }
     if(userid.length < 8 && avoid.test(userid) == false ){
         alert("YY/MM/DD 형식으로 입력해주세요.");
     }
