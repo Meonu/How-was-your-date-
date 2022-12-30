77588f8a4051c60d73ee24b68d65da47c7d80064
meonu <yeonugim42@gmail.com>
2022-12-30 10:25:28 +0900

    edit

--- post.js
+++ post.js
@@ -4,7 +4,7 @@ function check(){
 
     var avoid = /^[0-9]{2}\/[0-9]{2}\/[0-9]{2}$/;
 
-    if(userid.length != 0 && avoid.test(userid) == false ){
+    if(userid.length < 8 && avoid.test(userid) == false ){
         alert("22/12/25 형식으로 입력해주세요.");
     }
     var http = new XMLHttpRequest();
