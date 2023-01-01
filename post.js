93188f0a75691dd020f49a31744ff62f8247cc8b
Yeonwoo Kim <yeonwookim@Yeonwoos-MacBook-Air.local>
2023-01-01 11:51:02 +0900

    js edit

--- post.js
+++ post.js
@@ -3,6 +3,8 @@ function check(){
     var userpw = document.getElementById("how").value;
 
     var avoid = /^[0-9]{2}\/[0-9]{2}\/[0-9]{2}$/;
+    alert(document.getElementById("IDflag").value + document.getElementById("IDflag").value.length);
+
     if(document.getElementById("IDflag").value.length == 0)
     {
         alert("로그인이 필요합니다.");
